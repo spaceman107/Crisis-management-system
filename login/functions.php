77db61d31495d24d72fa7b13e50090476f15ca66
spaceman107 <75082536+spaceman107@users.noreply.github.com>
2023-11-25 22:54:03 +0200

    Update functions.php

--- login/functions.php
+++ login/functions.php
@@ -2,25 +2,24 @@
 
 function check_login($con)
 {
-if($_SESSION["id"])
-{
- $id= $_SESSION['id'];
- $query = "select * from login_info where id = '$id' limit 1  " ;
- 
- $result = mysqli_query($con,$query)  ;
- if($result && mysqli_num_rows($result) > 0 )
-  {
+    if($_SESSION["id"])
+    {
+         $id= $_SESSION['id'];
+         $query = "select * from login_info where id = '$id' limit 1  " ;
+         
+         $result = mysqli_query($con,$query);
 
-$user_data = mysqli_fetch_assoc($result);
-return $user_data ;
+         if($result && mysqli_num_rows($result) > 0 )
+          {
+               $user_data = mysqli_fetch_assoc($result);
+               return $user_data ;
+          } 
 
-  } 
+     }
 
- }
+    //redirect to login 
+    header("Location: login.php") ;
 
-//redirect to login 
-header("Location: login.php") ;
-
-die;
+    die;
 
 }
