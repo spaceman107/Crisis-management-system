3fe66fb1195d59b0acd2d6a74dd9ad7633918406
spaceman107 <75082536+spaceman107@users.noreply.github.com>
2023-12-05 16:08:01 +0200

    Add files via upload

--- Landing pages/admin_landing_page.php
+++ Landing pages/admin_landing_page.php
@@ -8,6 +8,11 @@ include "connection.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+
+     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+
     <style>
         html, body {
             height: 100%;
@@ -24,6 +29,10 @@ include "connection.php";
             color: white;
             text-align: center;
             padding: 10px;
+        }
+
+        #map { height: 80%; 
+               width: 80%;
         }
 
         section {
@@ -214,6 +223,10 @@ include "connection.php";
 
     </style>
 
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
 </head>
 <body>
 
@@ -286,6 +299,27 @@ include "connection.php";
     <section id="view-map">
         <h2>View Map</h2>
 
+        <div id="map"></div>
+
+         <script>
+
+         var map = L.map('map').setView([51.505, -55.09], 13);
+         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        
+        var popup = L.popup();
+
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("You clicked the map at " + e.latlng.toString())
+                .openOn(map);
+        }
+        map.on('click', onMapClick);
+
+        </script>
         <!-- Content for Stock Management goes here -->
     </section>
 
