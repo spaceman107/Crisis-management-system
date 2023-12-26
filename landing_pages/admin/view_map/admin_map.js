8e03e0c246b8bfa5518fcb1caf18d098b45fe370
teokon <74197733+teokon@users.noreply.github.com>
2023-12-26 18:14:33 +0200

    Update admin_map.js

--- landing_pages/admin/view_map/admin_map.js
+++ landing_pages/admin/view_map/admin_map.js
@@ -1,109 +1,172 @@
+// Define layers for different types of markers
+var offerLayer = L.layerGroup();
+var requestpendingLayer = L.layerGroup();
+var requestacceptedLayer = L.layerGroup();
+var baseLayer = L.layerGroup();
+var vehicleLayer = L.layerGroup();
 
-    var map = L.map('map').setView([37.983810, 23.727539], 11);
+var draggableMarkers = {};
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+// Create the map and add the tile layer
+const map = L.map('map', {
+    center: [37.983810, 23.727539],
+    zoom: 10,
+    layers: [requestpendingLayer, offerLayer, requestacceptedLayer, baseLayer, vehicleLayer]
+});
 
-        
-          fetch('Offer_coordinates.php')
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Â© OpenStreetMap contributors',
+}).addTo(map);
+
+
+// Create overlay maps for the layer control
+const overlays = {
+    "ACCEPTED REQUESTS": requestacceptedLayer,
+    "PENDING REQUESTS": requestpendingLayer,
+    "OFFERS": offerLayer,
+    "BASES": baseLayer,
+    "VEHICLES": vehicleLayer
+};
+
+// Add layer control to the map
+const layerControl = L.control.layers(null, overlays).addTo(map);
+
+
+fetch('offer_coordinates.php')
     .then(response => response.json())
     .then(OfferCoordinates => {
         console.log(OfferCoordinates);
-          
+
         OfferCoordinates.forEach(item => {
-          var markerOffer= L.divIcon({
-            html :'<img src="https://cdn-icons-png.flaticon.com/512/4151/4151073.png" width="30" height="30" alt="Custom Marker">',
-            className: 'markerOffer',
-            iconSize: [30, 20],     
-            });
- 
-            L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerOffer }).addTo(map).bindPopup("Offer.");
-          
-            });    
-        })
-    .catch(error => console.error('Error fetching data:', error));
-
-
-          ///REQUEST COORDINATES
-                                                                 
-    fetch('request_coordinates.php')
-    .then(response => response.json())
-    .then(RequestCoordinates => {
-        console.log(RequestCoordinates);
-
-       
-        RequestCoordinates.forEach(item => {
-            var markerRequest = L.divIcon({
+            var markerOffer = L.divIcon({
                 html: '<img src="https://cdn-icons-png.flaticon.com/512/4151/4151073.png" width="30" height="30" alt="Custom Marker">',
-                className: 'markerRequest',
+                className: 'markerOffer',
                 iconSize: [30, 20],
             });
 
-            L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerRequest }).addTo(map).bindPopup("Request.");
+            L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerOffer }).addTo(map).addTo(offerLayer).bindPopup("Offer.");
+
         });
     })
     .catch(error => console.error('Error fetching data:', error));
 
 
 
-    /// BASE COORDINATES
 
-    fetch('base_coordinates.php')
+///REQUEST PENDING COORDINATES
+
+fetch('request_coordinates_pending.php')
     .then(response => response.json())
-    .then(BaseCoordinates => {
-    console.log('BaseCoordinates:', BaseCoordinates);
+    .then(RequestCoordinates => {
+        console.log(RequestCoordinates);
 
-    BaseCoordinates.forEach(item => {
-    var markerBase = L.divIcon({
-        html: '<img src="https://cdn.iconscout.com/icon/free/png-256/free-base-1786434-1520324.png" width="30" height="30" alt="Custom Marker">',
-        className: 'markerBase',
-        iconSize: [30, 20],
-    });
 
-    var baseMarker = L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerBase, draggable: true }).addTo(map).bindPopup("Base id :" + item.base_id);
+        RequestCoordinates.forEach(item => {
+            var markerRequest = L.divIcon({
+                html: '<img src="https://image.pngaaa.com/232/2702232-middle.png" width="30" height="30" alt="Custom Marker">',
+                className: 'markerRequest',
+                iconSize: [30, 20],
+            });
 
-    // Event listener for dragend
-    baseMarker.on('dragend', function (event) {
-        var marker = event.target;
-        var position = marker.getLatLng();
-        var baseId = item.base_id;
-
-        // Ask for confirmation before updating the coordinates
-        if (confirm("Are you sure you want to update the coordinates?")) {
-            // Redirect to the PHP script with updated coordinates
-            updateCoordinates(baseId, position.lat, position.lng);
-        } else {
-            // Revert the marker position if the user cancels
-            marker.setLatLng(new L.LatLng(item.lat, item.lng));
-        }
-    });
- 
-    draggableMarkers[item.base_id] = baseMarker;
-    });
+            L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerRequest })
+                .addTo(requestpendingLayer)
+                .bindPopup(
+                    "User Fullname: " + item.first_name + " " + item.last_name +
+                    "<br>Phone: " + item.phone +
+                    "<br>Time of Submission: " + item.time_of_submition +
+                    "<br>Product Category: " + item.name_category +
+                    "<br>Vehicle Name: " + item.vehicle_name);
+        });
     })
     .catch(error => console.error('Error fetching data:', error));
-    function updateCoordinates(baseId, lat, lng) {
+
+///REQUEST ACCEPTED COORDINATES
+
+fetch('request_coordinates_accepted.php')
+    .then(response => response.json())
+    .then(RequestAcceptedCoordinates => {
+        console.log(RequestAcceptedCoordinates);
+
+
+        RequestAcceptedCoordinates.forEach(item => {
+            var markerAcceptedRequest = L.divIcon({
+                html: '<img src="https://cdn-icons-png.flaticon.com/512/4151/4151073.png" width="30" height="30" alt="Custom Marker">',
+                className: 'markerAcceptedRequest',
+                iconSize: [30, 20],
+            });
+
+            L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerAcceptedRequest })
+
+                .addTo(requestacceptedLayer)
+                .bindPopup(
+                    "User Fullname: " + item.first_name + " " + item.last_name +
+                    "<br>Phone: " + item.phone +
+                    "<br>Time of Submission: " + item.time_of_submition +
+                    "<br>Product Category: " + item.name_category +
+                    "<br>Vehicle Name: " + item.vehicle_name);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+
+/// BASE COORDINATES
+
+fetch('base_coordinates.php')
+    .then(response => response.json())
+    .then(BaseCoordinates => {
+        console.log('BaseCoordinates:', BaseCoordinates);
+
+        BaseCoordinates.forEach(item => {
+            var markerBase = L.divIcon({
+                html: '<img src="https://cdn.iconscout.com/icon/free/png-256/free-base-1786434-1520324.png" width="30" height="30" alt="Custom Marker">',
+                className: 'markerBase',
+                iconSize: [30, 20],
+            });
+
+            var baseMarker = L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerBase, draggable: true }).addTo(map).addTo(baseLayer).bindPopup("Base id :" + item.base_id);
+
+            // Event listener for dragend
+            baseMarker.on('dragend', function(event) {
+                var marker = event.target;
+                var position = marker.getLatLng();
+                var baseId = item.base_id;
+
+                // Ask for confirmation before updating the coordinates
+                if (confirm("Are you sure you want to update the coordinates?")) {
+                    // Redirect to the PHP script with updated coordinates
+                    updateCoordinates(baseId, position.lat, position.lng);
+                } else {
+                    // Revert the marker position if the user cancels
+                    marker.setLatLng(new L.LatLng(item.lat, item.lng));
+                }
+            });
+
+            draggableMarkers[item.base_id] = baseMarker;
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+function updateCoordinates(baseId, lat, lng) {
     // Use Fetch API to send a POST request to the PHP script
     fetch('update_base_coordinates.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `baseId=${baseId}&lat=${lat}&lng=${lng}`,
-    })
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `baseId=${baseId}&lat=${lat}&lng=${lng}`,
+        })
         .then(response => response.json())
         .then(data => {
             if (data.status === 'success') {
-                alert('Coordinates updated successfully');
+                alert('Rescuer added successfully');
             } else {
-                alert('Error updating coordinates');
+                alert('Error');
             }
         })
         .catch(error => {
             console.error('Error:', error);
         });
-    }
+}
 
 
 // VEHICLE COORDINATES
@@ -135,9 +198,9 @@ fetch('vehicle_coordinates.php')
             }
 
             L.marker([parseFloat(item.lat), parseFloat(item.lng)], { icon: markerVehicle })
-                .addTo(map)  // Make sure 'map' is defined and initialized before this line
+
+                .addTo(vehicleLayer)
                 .bindPopup(popupContent);
         });
     })
     .catch(error => console.error('Error fetching data:', error));
-
