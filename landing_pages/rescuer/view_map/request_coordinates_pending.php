d2e55121e4d184a21df30c7f3f776482dfb0fb91
teokon <74197733+teokon@users.noreply.github.com>
2024-02-05 19:37:28 +0200

    Add files via upload

--- landing_pages/rescuer/view_map/request_coordinates_pending.php
+++ landing_pages/rescuer/view_map/request_coordinates_pending.php
@@ -2,22 +2,21 @@
 session_start();
 include("connection.php");
 include("functions.php");
-// COORDINATES FOR USER REQUEST (PINS)
+
 $RequestCoordinatesQuery = " SELECT x_coordinate AS lat, y_coordinate AS lng ,user.user_id   ,
 user.first_name ,
 user.last_name,
 user.phone, 
 transaction.time_of_submition,
 transaction.transaction_id,
-product_type.name_category,
-vehicle.vehicle_name
+transaction.quantity,
+product_type.name_category
+
     FROM location 
     INNER JOIN user  ON location.location_id = user.location_id 
     INNER JOIN transaction ON user.user_id = transaction.user_id 
     INNER JOIN  product ON transaction.product_id = product.product_id 
     INNER JOIN  product_type ON product.product_category = product_type.product_category_id
-    INNER JOIN task ON transaction.transaction_id = task.transaction_id
-    INNER JOIN vehicle ON task.vehicle_id = vehicle.vehicle_id  
     WHERE (transaction.type = 'REQUEST' AND transaction.status = 'PENDING')";
 $RequestCoordinatesResult = mysqli_query($con, $RequestCoordinatesQuery);
 
