2614d3a325cfdf29227b4313d6b14ad2728f1a9c
spaceman107 <75082536+spaceman107@users.noreply.github.com>
2024-02-09 01:55:07 +0200

    Update get_announcements.php

--- landing_pages/user/announcements/get_announcements.php
+++ landing_pages/user/announcements/get_announcements.php
@@ -1,21 +1,6 @@
-
 <?php
-
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "crisis management";
-
-
-
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-// Create connection
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-
+session_start();
+include("../../../login/connection.php");
 
 $query = "SELECT a.announcement_id, a.description,GROUP_CONCAT(ap.product_id SEPARATOR ', ') AS pro_id, GROUP_CONCAT(p.product_name SEPARATOR ', ') AS products
           FROM announcement a
@@ -23,10 +8,10 @@ $query = "SELECT a.announcement_id, a.description,GROUP_CONCAT(ap.product_id SEP
           LEFT JOIN product p ON ap.product_id = p.product_id
           GROUP BY a.announcement_id";
 
-$result = mysqli_query($connection, $query);
+$result = mysqli_query($con, $query);
 
 if (!$result) {
-    die("Query failed: " . mysqli_error($connection));
+    die("Query failed: " . mysqli_error($con));
 }
 
 $announcements = mysqli_fetch_all($result, MYSQLI_ASSOC);
