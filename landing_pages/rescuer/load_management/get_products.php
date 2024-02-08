05fa2ae5ba5677ca5382643153f9226a53baa2f1
teokon <74197733+teokon@users.noreply.github.com>
2024-02-08 19:40:25 +0200

    Update get_products.php

--- landing_pages/rescuer/load_management/get_products.php
+++ landing_pages/rescuer/load_management/get_products.php
@@ -3,12 +3,12 @@ session_start();
 include("connection.php");
 
 
-// Check the connection
+
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
-// Fetch products from the database using an SQL query
+
 $sql = "SELECT product_id, product_name , quantity FROM product";
 $result = $con->query($sql);
 
@@ -19,15 +19,15 @@ if ($result->num_rows > 0) {
         $products[] = $row;
     }
 
-    // Return the products as JSON
+  
     header('Content-Type: application/json');
     echo json_encode($products);
 } else {
-    // Return an empty array if no products are found
+   
     header('Content-Type: application/json');
     echo json_encode(array());
 }
 
-// Close the database connection
+
 $con->close();
 ?>
