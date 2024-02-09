782b1501857aa75cba01240fe7ba9c5adca499ba
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:23:37 +0200

    Update fetch_categories.php

--- landing_pages/user/new_request/fetch_categories.php
+++ landing_pages/user/new_request/fetch_categories.php
@@ -3,19 +3,15 @@ session_start();
 include("../../../login/connection.php");
 }
 
-//select categories from the database
 $sql = "SELECT * FROM product_type";
 $result = mysqli_query($con, $sql);
 
-//check for errors in the query
 if (!$result) {
     die("Error: " . mysqli_error($conn));
 }
 
-//save associative array
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-//return the categories as JSON
 header('Content-Type: application/json');
 echo json_encode($categories);
 
