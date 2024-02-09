50905fd4328923039c75c03066eec471529de207
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:20:25 +0200

    Update update_base_coordinates.php

--- landing_pages/admin/view_map/update_base_coordinates.php
+++ landing_pages/admin/view_map/update_base_coordinates.php
@@ -2,29 +2,28 @@
 session_start();
 include("connection.php");
 
-// Check if the required parameters are set
+
 if (isset($_POST['locationId'], $_POST['lat'], $_POST['lng'])) {
     // Sanitize and retrieve the parameters
     $locationId = $_POST['locationId']; // Convert to integer
     $lat = $_POST['lat'];
     $lng = $_POST['lng'];
 
-    // Update the database
+ 
     $updateQuery = "UPDATE location SET x_coordinate = ?, y_coordinate = ? WHERE location_id = ?";
     $stmt = mysqli_prepare($con, $updateQuery);
 
-    // Check for errors in preparing the statement
+
     if ($stmt === false) {
         die('Error preparing statement');
     }
 
-    // Bind parameters
+    
     mysqli_stmt_bind_param($stmt, "ddi", $lat, $lng, $locationId);
 
-    // Execute the statement
+   
     $result = mysqli_stmt_execute($stmt);
 
-    // Check for errors in executing the statement
     if ($result === false) {
         die('Error executing statement: ' . mysqli_stmt_error($stmt));
         echo json_encode($response);
@@ -32,10 +31,10 @@ if (isset($_POST['locationId'], $_POST['lat'], $_POST['lng'])) {
 
     mysqli_stmt_close($stmt);
 
-    // Success response
+
     header("Location: admin_page.php");
 } else {
-    // Invalid or missing parameters
+
     $response = array('status' => 'error', 'message' => 'Invalid or missing parameters');
     echo json_encode($response);
 }
