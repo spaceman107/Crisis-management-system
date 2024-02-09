42a777346ce0e00f64dd56a1cef862988a5c5837
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 17:52:39 +0200

    Update admin_landing_page.php

--- landing_pages/admin/admin_landing_page.php
+++ landing_pages/admin/admin_landing_page.php
@@ -23,7 +23,7 @@ include("../../login/connection.php");
     <header>
         <button class="openbtn" onclick="openNav()">&#9776; Open Sidebar</button>
         <h2>Admin Dashboard</h2>
-        <a class="logout-btn" href="#">Logout</a>
+        <a class="logout-btn" href="../login/logout.php">Logout</a>
     </header>
     <div id="mySidebar" class="sidebar">
         <nav>
