6cef1ec4d42335d1ebd383b8524858aecbb10b10
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:15:02 +0200

    Update mark_items_available.php

--- landing_pages/admin/stock_management/mark_items_available.php
+++ landing_pages/admin/stock_management/mark_items_available.php
@@ -8,17 +8,17 @@ ini_set('display_errors', '1');
 $jsonData = file_get_contents('php://input');
 error_log($jsonData, 3, 'errors.log');
 $data = json_decode($jsonData);
-$result = array(); // Initialize $result before the loop
+$result = array();
 $result['success'] = true;
 
 if ($data === NULL && json_last_error() !== JSON_ERROR_NONE) {
     $result['success'] = false;
     $result['message'] = 'Error decoding JSON data.';
-} elseif (isset($data->productIds)) {// Get the selected items array
+} elseif (isset($data->productIds)) {
     $productIds = $data->productIds;
 
     foreach ($productIds as $productId) {
-        // Perform operations with $productId as needed
+        
         $query = "UPDATE product SET availability = 'YES' WHERE product_id = $productId";
 
         if ($con->query($query) !== TRUE) {
