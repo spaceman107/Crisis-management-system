5a9f98d1c3ba570152400fb500eaac4da69b602a
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:13:46 +0200

    Update fetch_categories.php

--- landing_pages/admin/stock_management/fetch_categories.php
+++ landing_pages/admin/stock_management/fetch_categories.php
@@ -8,7 +8,7 @@ ini_set('display_errors', '1');
 $query = "SELECT product_category_id, name_category FROM product_type";
 $result = $con->query($query);
 
-// Fetch categories as an associative array
+
 $categories = array();
 while ($row = $result->fetch_assoc()) {
     $categories[] = $row;
