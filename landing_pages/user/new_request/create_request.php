1b20ca06e2ef5b117674f9a94a8a1a7461cdbe6d
spaceman107 <75082536+spaceman107@users.noreply.github.com>
2024-02-09 02:03:51 +0200

    Update create_request.php

--- landing_pages/user/new_request/create_request.php
+++ landing_pages/user/new_request/create_request.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("../../../login/connection.php");
-
+$logged_in_user_id = $_SESSION['user_id'];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $selectedProducts = $_POST['products'];
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     for ($i = 0; $i < count($selectedProducts); $i++) {
         $product = $selectedProducts[$i];
         
-        $sql = "INSERT INTO transaction (transaction_id, user_id,product_id,number_of_people_in_need,status, type, quantity) VALUES (20,4,$product,$numPeople,'PENDING','REQUEST', '10')";
+        $sql = "INSERT INTO transaction (user_id,product_id,number_of_people_in_need,status, type, quantity) VALUES ($logged_in_user_id,$product,$numPeople,'PENDING','REQUEST', '10')";
                    
             mysqli_query($con, $sql);
 
