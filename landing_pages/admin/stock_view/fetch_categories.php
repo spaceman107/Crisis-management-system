ea7e50cd19bee54015e3feef43e27891052bb6e9
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:16:22 +0200

    Update fetch_categories.php

--- landing_pages/admin/stock_view/fetch_categories.php
+++ landing_pages/admin/stock_view/fetch_categories.php
@@ -2,23 +2,23 @@
 session_start();
 include("../../../login/connection.php");
 
-//fetch categories names from the database
+
 $sql = "SELECT * FROM product_type";
 $result = mysqli_query($con, $sql);
 
-//catch errors in the query
+
 if (!$result) {
     die("Error: " . mysqli_error($con));
 }
 
-//turn the results in an associative array
+
 $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-//use the assosiative array to return the products as JSON
+
 header('Content-Type: application/json');
 echo json_encode($categories);
 
-//close the con
+
 mysqli_close($con);
 ?>
 
