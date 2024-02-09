89687be6c308514a17893ac2ddb8a0eb894a042e
spaceman107 <75082536+spaceman107@users.noreply.github.com>
2024-02-09 02:20:04 +0200

    Update fetch_categories.php

--- landing_pages/user/new_request/fetch_categories.php
+++ landing_pages/user/new_request/fetch_categories.php
@@ -3,11 +3,11 @@ session_start();
 include("../../../login/connection.php");
 }
 
-// Fetch category names from the database
+//select categories from the database
 $sql = "SELECT * FROM product_type";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($con, $sql);
 
-// Check for errors in the query
+//check for errors in the query
 if (!$result) {
     die("Error: " . mysqli_error($conn));
 }
@@ -19,7 +19,6 @@ $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 header('Content-Type: application/json');
 echo json_encode($categories);
 
-// Close the connection
-mysqli_close($conn);
+mysqli_close($con);
 ?>
 
