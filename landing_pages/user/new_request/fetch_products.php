1bdf05bf93a9b437f6069e4bfdf4ab03e70911f8
teokon <74197733+teokon@users.noreply.github.com>
2024-02-09 11:24:33 +0200

    Update fetch_products.php

--- landing_pages/user/new_request/fetch_products.php
+++ landing_pages/user/new_request/fetch_products.php
@@ -2,10 +2,10 @@
 session_start();
 include("../../../login/connection.php");
 
-//decode the selected categories from the request parameter
+
 $selectedCategories = isset($_GET['category']) ? explode(',', $_GET['category']) : ['all'];
 
-//check if all is in the selected category
+
 if (in_array('all', $selectedCategories)) {
     $sql = "SELECT p.*,product_type.name_category AS category, COALESCE(SUM(t.quantity), 0) AS total_quantity_in_transactions
             FROM product p
@@ -14,7 +14,7 @@ if (in_array('all', $selectedCategories)) {
             WHERE t.status = 'ACCEPTED' OR t.status IS NULL
             GROUP BY p.product_id";
 } else {
-    // using IN clause to handle multiple categories
+ 
     $selectedCategoriesString = implode(',', $selectedCategories);
 
     $sql = "SELECT p.*,product_type.name_category AS category, COALESCE(SUM(t.quantity), 0) AS total_quantity_in_transactions
@@ -30,18 +30,18 @@ if (in_array('all', $selectedCategories)) {
 
 $result = mysqli_query($con, $sql);
 
-//catch errors in the query
+
 if (!$result) {
     die("Error: " . mysqli_error($con));
 }
 
-// turn the results in an associative array
+
 $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-// use the assosiative array to return the products as JSON
+
 header('Content-Type: application/json');
 echo json_encode($products);
 
-//close the con
+
 mysqli_close($con);
 ?>
